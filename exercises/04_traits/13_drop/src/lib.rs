067f049a65cbd3f3eb7f95649bb4eccaba66b306
Santosh <santosh.pandeyy@gmail.com>
2024-07-16 17:06:46 +0530

    further-changes

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,35 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    pub to_be_dropped: bool,
+    pub defused_invoked: bool
+}
+
+impl DropBomb {
+    //const mut check = true;
+    //to_be_dropped_1 = true;
+    
+    pub fn new() -> DropBomb {
+        let cur_bomb = DropBomb{
+            to_be_dropped: true,
+            defused_invoked: false
+        };
+        cur_bomb
+    }
+
+    pub fn drop(&mut self) {
+        if self.to_be_dropped && !self.defused_invoked {
+            panic!("Boom")
+        }
+    }
+
+    pub fn defuse(&mut self) {
+        self.to_be_dropped = false;
+        self.defused_invoked = true;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,7 +38,8 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_drop_bomb() {
-        let bomb = DropBomb::new();
+        let mut bomb = DropBomb::new();
+        bomb.drop();
         // The bomb should panic when dropped
     }
 
@@ -17,6 +47,8 @@ mod tests {
     fn test_defused_drop_bomb() {
         let mut bomb = DropBomb::new();
         bomb.defuse();
+        println!("{}", bomb.to_be_dropped);
+        bomb.drop();
         // The bomb should not panic when dropped
         // since it has been defused
     }
